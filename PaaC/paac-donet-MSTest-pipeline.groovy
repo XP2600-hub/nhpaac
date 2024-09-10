87345258775e64583c09bad40c8caf2ff2ae72cc
XP2600-hub <77234446+XP2600-hub@users.noreply.github.com>
2024-09-10 14:51:57 +0300

    push dotnet pipeline

---
+++ PaaC/paac-donet-MSTest-pipeline.groovy
@@ -0,0 +1,42 @@
+pipeline {
+  // This pipeline requires the following plugins:
+  // * Git: https://plugins.jenkins.io/git/
+  // * Workflow Aggregator: https://plugins.jenkins.io/workflow-aggregator/
+  // * MSTest: https://plugins.jenkins.io/mstest/
+  agent 'any'
+  stages {
+    stage('Environment') {
+      steps {
+          echo "PATH = ${PATH}"
+      }
+    }
+    stage('Checkout') {
+      steps {
+
+        script {
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/XP2600-hub/nh-java-testme.git']]])
+        }
+      }
+    }
+    stage('Dependencies') {
+      steps {
+        sh(script: 'dotnet restore')
+      }
+    }
+    stage('Build') {
+      steps {
+        sh(script: 'dotnet build --configuration Release', returnStdout: true)
+      }
+    }
+    stage('Test') {
+      steps {
+        sh(script: 'dotnet test -l:trx || true')        
+      }
+    }
+  }
+  post {
+    always {
+      mstest(testResultsFile: '**/*.trx', failOnError: false, keepLongStdio: true)
+    }
+  }
+}
